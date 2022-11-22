b588050348dd8f04761552baad5f206e0b67606b
Brian Alegre <brialegre@yahoo.com>
2022-11-21 21:24:31 -0800

    Updated Notes: PageModel

--- PageModel/Pizza.cshtml.cs
+++ PageModel/Pizza.cshtml.cs
@@ -1,20 +1,37 @@
-@page
-@model PizzaModel
-@{
-}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
-<h1 class="text-center">Codecademy Pizza</h1>
-<br>
-<div class="card mx-auto" style="width: 18rem;">
-  <img src="https://image.flaticon.com/icons/svg/1404/1404945.svg" class="card-img-top" alt="pizza icon">
-  <div class="card-body">
-    <h4 class="card-title text-center">Confirm your order</h4>
-    <br>
-		<h5>Pizza for: @Model.Customer</h5>
-		<h5>Order: @Model.Order</h5>
-    <h5>Extra Cheese: @Model.ExtraCheese</h5>
-		<h5>Total: $@String.Format("{0:0.00}", Model.Total)</h5>
-    <br>
-    <a href="#" class="btn btn-primary">Place Order</a>
-  </div>
-</div>
+namespace starting_app.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public double PizzaTotal(string pizzaType)
+        {
+            Dictionary<string, double> PizzaCost = new Dictionary<string, double>()
+      {
+        { "Cheese", 10.00 },
+        { "Pepperoni", 11.00 },
+        { "Vegetarian", 12.00 },
+      };
+
+            return PizzaCost[pizzaType];
+        }
+
+        public string Customer { get; set; }
+        public string Order { get; set; }
+        public bool ExtraCheese { get; set; }
+        public double Total { get; set; }
+
+        public void OnGet()
+        {
+            Customer = "John Doe";
+            Order = "Cheese";
+            ExtraCheese = false;
+            Total = PizzaTotal("Cheese");
+        }
+    }
+}
