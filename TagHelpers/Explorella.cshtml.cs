5f777744a6ce3c80b11a2200e0fdf88da0ca718a
Brian Alegre <brialegre@yahoo.com>
2022-11-28 21:54:11 -0800

    Updated Notes: TagHelpers

--- TagHelpers/Explorella.cshtml.cs
+++ TagHelpers/Explorella.cshtml.cs
@@ -7,11 +7,26 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MyApp.Namespace
 {
-  public class ExplorellaModel : PageModel
-  {
-    public string Country { get; set; }
-    public void OnGet()
+    public class ExplorellaModel : PageModel
     {
+        public string Country { get; set; }
+        public void OnGet()
+        {
+            // Assign Properties
+            Countries = new List<SelectListItem>
+        {
+          new SelectListItem {Value = "AR", Text = "Argentina"},
+          new SelectListItem {Value = "FR", Text = "France"},
+          new SelectListItem {Value = "BR", Text = "Brazil"},
+          new SelectListItem {Value = "GER", Text = "Germany"},
+          new SelectListItem {Value = "CHI", Text = "China"},
+        };
+        }
+
+        // Create a List 
+        // Add Getter and Setter
+        public List<SelectListItem> Countries
+        { get; set; }
+
     }
-  }
 }
