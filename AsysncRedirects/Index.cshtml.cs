fdedc1f2fa3436eb1b0151c1062bf21b7f2ab836
Brian Alegre <brialegre@yahoo.com>
2022-12-20 20:14:27 -0800

    Updated Notes: AsyncRedirects

--- AsysncRedirects/Index.cshtml.cs
+++ AsysncRedirects/Index.cshtml.cs
@@ -10,21 +10,21 @@ using Zoogle.Models;
 
 namespace Zoogle.Pages
 {
-    public class IndexModel : PageModel
+  public class IndexModel : PageModel
+  {
+    public List<Animal> Animals { get; set; }
+
+    public async Task<IActionResult> OnGetAsync()
     {
-        public List<Animal> Animals { get; set; }
+      Animals = Zoo.Animals;
 
-        public IActionResult OnGet()
-        {
-            Animals = Zoo.Animals;
+      // Asynchronous code
+      using (StreamWriter writer = new StreamWriter("log.txt", append: true))
+      {
+       await writer.WriteLineAsync($"Zoogle visit recorded at {DateTime.Now}.");
+      }
 
-            // Asynchronous code
-            using (StreamWriter writer = new StreamWriter("log.txt", append: true))
-            {
-                await writer.WriteLineAsync($"Zoogle visit recorded at {DateTime.Now}.");
-            }
-
-            return Page();
-        }
+      return Page();
     }
+  }
 }
