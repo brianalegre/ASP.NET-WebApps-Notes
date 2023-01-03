a496d941b51e0d0046993f3baf7541e861ddbb1e
Brian Alegre <brialegre@yahoo.com>
2023-01-02 21:58:51 -0800

    updated notes

--- WireEF/Program.cs
+++ WireEF/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using RazorCountry.Data;
 
 namespace RazorCountry
 {
@@ -13,7 +15,19 @@ namespace RazorCountry
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            CreateDbIfNotExists(host);
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<CountryContext>();
+                context.Database.EnsureCreated();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
