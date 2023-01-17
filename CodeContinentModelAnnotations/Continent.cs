82d16d0eb0971fd47e1ee565daec2fb7c3bfbda9
Brian Alegre <brialegre@yahoo.com>
2023-01-16 18:59:45 -0800

    Updated Notes

--- CodeContinentModelAnnotations/Continent.cs
+++ CodeContinentModelAnnotations/Continent.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RazorCountry.Models
 {
-  public class Continent
-  {
-    public string ID { get; set; }
-    public string Name { get; set; }
-  }
+    public class Continent
+    {
+        [Required, StringLength(2, MinimumLength = 2), Display(Name = "Code")]
+        [RegularExpression(@"[A-Z]+", ErrorMessage = "Only upper case characters are allowed.")]
+
+        public string ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
 }
